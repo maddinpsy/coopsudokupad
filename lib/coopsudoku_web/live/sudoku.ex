f38be0943d0f4665629d76800b8ef6ef9378e77f
Martin <maddinpsycho@gmail.com>
2024-10-13 22:19:32 +0200

    add select

--- lib/coopsudoku_web/live/sudoku.ex
+++ lib/coopsudoku_web/live/sudoku.ex
@@ -1,8 +1,18 @@
 defmodule CoopsudokuWeb.Sudoku do
   use CoopsudokuWeb, :live_view
+  def id(r, c), do: 100 * r + c
 
   def mount(_params, _session, socket) do
-    cells = for c <- 1..9, r <- 1..9, do: %{row: r, col: c}
+    cells =
+      for c <- 1..9, r <- 1..9, into: %{}, do: {id(r, c), %{row: r, col: c, selected: false}}
+
     {:ok, assign(socket, cells: cells)}
+  end
+
+  def handle_event("select", %{"row" => row, "col" => col, "altKey" => alt}, socket) do
+    {r, _} = Integer.parse(row)
+    {c, _} = Integer.parse(col)
+    new_cells = socket.assigns.cells |> update_in([id(r, c), :selected], &(not &1))
+    {:noreply, assign(socket, cells: new_cells)}
   end
 end
