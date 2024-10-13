f26b8339c05902b98a8adbb7b794aad60c3a5ce6
Martin <maddinpsycho@gmail.com>
2024-10-13 20:11:15 +0200

    create live view

---
+++ lib/coopsudoku_web/live/message.ex
@@ -0,0 +1,7 @@
+defmodule CoopsudokuWeb.Message do
+  use CoopsudokuWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/coopsudoku_web/router.ex
+++ lib/coopsudoku_web/router.ex
@@ -17,7 +17,7 @@ defmodule CoopsudokuWeb.Router do
   scope "/", CoopsudokuWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", Message
   end
 
   # Other scopes may use custom stacks.
