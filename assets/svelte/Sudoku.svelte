ad7b0d46368f4425b80622de214e9dfbf305a662
Martin Markmann <martin.markmann@viakom.de>
2024-10-14 20:59:03 +0200

    add drag drop select

--- assets/svelte/Sudoku.svelte
+++ assets/svelte/Sudoku.svelte
@@ -2,22 +2,47 @@
   export let cells;
   export let live;
 
-  function select(row, col) {
-    live.pushEvent("select", { row: row, col: col }, () => {});
+  let select_mode = "nothing";
+
+  function down(altKey, cell) {
+    if (!altKey) {
+      live.pushEvent("clear_selection", {}, () => {});
+    }
+    if (!cell.selected) {
+      select_mode = "selecting";
+      live.pushEvent("select", cell, () => {});
+    } else {
+      select_mode = "deselecting";
+      live.pushEvent("deselect", cell, () => {});
+    }
+  }
+
+  function move(cell) {
+    if (select_mode == "selecting" && !cell.selected) {
+      live.pushEvent("select", cell, () => {});
+    }
+    if (select_mode == "deselecting" && cell.selected) {
+      live.pushEvent("deselect", cell, () => {});
+    }
   }
 </script>
 
 <h1>LiveView Message Page</h1>
 <div
   class="grid grid-cols-9 grid-rows-9 max-w-full aspect-square p-0.5 bg-black gap-0.5"
+  draggable="false"
+  on:pointerup={() => (select_mode = "nothing")}
 >
   {#each cells as cell}
     <!-- svelte-ignore a11y-no-static-element-interactions -->
     <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <!-- on:click={() => select(cell.row, cell.col)} -->
     <div
-      on:click={() => select(cell.row, cell.col)}
+      on:pointerdown={(e) => down(e.shiftKey || e.metaKey || e.ctrlKey, cell)}
+      on:pointermove={(e) => move(cell)}
       class="row-start-{cell.row} col-start-{cell.col} bg-white border-lime-500 border-4"
       style:border-style={cell.selected ? "solid" : "none"}
+      draggable="false"
     ></div>
     <!-- style="border-style: solid; border-width: 0.4em; border-image: conic-gradient(from 30deg, red 0% 50%, blue 50% 100%) 1" -->
     <!-- style="box-shadow: inset 0 0 0 0.3em red, inset 0 0 0 0.6em blue" -->
--- lib/coopsudoku_web/live/sudoku.ex
+++ lib/coopsudoku_web/live/sudoku.ex
@@ -17,13 +17,32 @@ defmodule CoopsudokuWeb.Sudoku do
 
   def handle_event("select", %{"row" => r, "col" => c}, socket) do
     id = id(r, c)
-    new_cells = socket.assigns.cells |> update_in([id, :selected], &(not &1))
+    new_cells = socket.assigns.cells |> put_in([id, :selected], true)
 
     CoopsudokuWeb.Endpoint.broadcast(topic(), "message", %{
       id: id,
       selected: new_cells[id].selected
       # name: socket.assigns.username
     })
+
+    {:noreply, assign(socket, cells: new_cells)}
+  end
+
+  def handle_event("deselect", %{"row" => r, "col" => c}, socket) do
+    id = id(r, c)
+    new_cells = socket.assigns.cells |> put_in([id, :selected], false)
+    {:noreply, assign(socket, cells: new_cells)}
+  end
+
+  def handle_event("clear_selection", _, socket) do
+    new_cells =
+      socket.assigns.cells
+      |> Map.to_list()
+      |> Enum.filter(fn {_, v} -> v.selected end)
+      |> Enum.reduce(
+        socket.assigns.cells,
+        fn {k, _}, acc -> put_in(acc, [k, :selected], false) end
+      )
 
     {:noreply, assign(socket, cells: new_cells)}
   end
