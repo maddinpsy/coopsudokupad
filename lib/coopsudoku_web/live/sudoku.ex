7f4e934f83ebc51733c12af586f1897051ff5e27
Martin Markmann <martin.markmann@viakom.de>
2024-11-01 18:56:57 +0100

    fmt

--- lib/coopsudoku_web/live/sudoku.ex
+++ lib/coopsudoku_web/live/sudoku.ex
@@ -4,24 +4,30 @@ defmodule CoopsudokuWeb.Sudoku do
 
   def id(r, c), do: 100 * r + c
 
-  def mount( %{"name" => _, "color" => _, "room" => _} = params, _session, socket) do
+  def mount(%{"name" => _, "color" => _, "room" => _} = params, _session, socket) do
     if connected?(socket) do
       CoopsudokuWeb.Endpoint.subscribe(topic(params["room"]))
     end
 
-    :ets.insert(:room_users,{params["room"], socket.id})
+    :ets.insert(:room_users, {params["room"], socket.id})
 
     cells = sync_room(params["room"])
 
-    {:ok, assign(socket, cells: cells, name: params["name"], color: params["color"], room: params["room"])}
+    {:ok,
+     assign(socket,
+       cells: cells,
+       name: params["name"],
+       color: params["color"],
+       room: params["room"]
+     )}
   end
 
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
 
-  def sync_room(room)do
-    case :ets.lookup(:sudoku_data,room) do
+  def sync_room(room) do
+    case :ets.lookup(:sudoku_data, room) do
       [{_, data}] -> data
       [] -> for c <- 1..9, r <- 1..9, into: %{}, do: {id(r, c), %{row: r, col: c, selected: []}}
     end
@@ -29,9 +35,11 @@ defmodule CoopsudokuWeb.Sudoku do
 
   def handle_event("select", %{"row" => r, "col" => c}, socket) do
     id = id(r, c)
-    new_cells = socket.assigns.cells |> update_in([id, :selected], &set_add(&1,socket.assigns.color))
 
-    :ets.insert(:sudoku_data,{socket.assigns.room, new_cells})
+    new_cells =
+      socket.assigns.cells |> update_in([id, :selected], &set_add(&1, socket.assigns.color))
+
+    :ets.insert(:sudoku_data, {socket.assigns.room, new_cells})
 
     CoopsudokuWeb.Endpoint.broadcast(topic(socket.assigns.room), "sync_required", %{})
 
@@ -40,9 +48,11 @@ defmodule CoopsudokuWeb.Sudoku do
 
   def handle_event("deselect", %{"row" => r, "col" => c}, socket) do
     id = id(r, c)
-    new_cells = socket.assigns.cells |> update_in([id, :selected], &set_delete(&1,socket.assigns.color))
 
-    :ets.insert(:sudoku_data,{socket.assigns.room, new_cells})
+    new_cells =
+      socket.assigns.cells |> update_in([id, :selected], &set_delete(&1, socket.assigns.color))
+
+    :ets.insert(:sudoku_data, {socket.assigns.room, new_cells})
 
     CoopsudokuWeb.Endpoint.broadcast(topic(socket.assigns.room), "sync_required", %{})
 
@@ -55,12 +65,14 @@ defmodule CoopsudokuWeb.Sudoku do
       |> Map.to_list()
       |> Enum.reduce(
         socket.assigns.cells,
-        fn {cell_id, _}, acc -> update_in(acc, [cell_id, :selected], &set_delete(&1,socket.assigns.color)) end
+        fn {cell_id, _}, acc ->
+          update_in(acc, [cell_id, :selected], &set_delete(&1, socket.assigns.color))
+        end
       )
 
-      :ets.insert(:sudoku_data,{socket.assigns.room, new_cells})
+    :ets.insert(:sudoku_data, {socket.assigns.room, new_cells})
 
-      CoopsudokuWeb.Endpoint.broadcast(topic(socket.assigns.room), "sync_required", %{})
+    CoopsudokuWeb.Endpoint.broadcast(topic(socket.assigns.room), "sync_required", %{})
 
     {:noreply, assign(socket, cells: new_cells)}
   end
@@ -71,15 +83,15 @@ defmodule CoopsudokuWeb.Sudoku do
   end
 
   defp topic(room) do
-    "chat"<>room
+    "chat" <> room
   end
 
   def set_add(list, value) do
-    if Enum.member?(list,value), do: list, else: [value | list]
+    if Enum.member?(list, value), do: list, else: [value | list]
   end
 
   def set_delete(list, value) do
-    Enum.reject(list,fn elm -> elm==value end)
+    Enum.reject(list, fn elm -> elm == value end)
   end
 
   def render(%{name: _} = assigns) do
@@ -95,10 +107,11 @@ defmodule CoopsudokuWeb.Sudoku do
   end
 
   def terminate(_reason, socket) do
-    handle_event("clear_selection",[],socket)
-    :ets.delete_object(:room_users,{socket.assigns.room, socket.id})
-    if(:ets.lookup(:room_users,socket.assigns.room) |> length == 0) do
-      :ets.delete(:sudoku_data,socket.assigns.room)
+    handle_event("clear_selection", [], socket)
+    :ets.delete_object(:room_users, {socket.assigns.room, socket.id})
+
+    if(:ets.lookup(:room_users, socket.assigns.room) |> length == 0) do
+      :ets.delete(:sudoku_data, socket.assigns.room)
     end
   end
 end
