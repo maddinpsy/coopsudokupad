de8331957bdc7427b0d1d2aa1eee20982afdd9a7
Martin <maddinpsycho@gmail.com>
2024-11-27 19:52:12 +0100

    remove elixir

--- functions/api.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(request) {
-    const url = new URL(request.url);
-    url.hostname = 'another-domain.de';
-    const response = await fetch(url)
-    return new Response(response.json)
-}
--- lib/coopsudoku/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Coopsudoku.Mailer do
-  use Swoosh.Mailer, otp_app: :coopsudoku
-end
